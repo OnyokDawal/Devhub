2d0ac11f8a105f60a637a9c4468af4b8002eefd8
OnyokDawal <junjun061096@gmail.com>
2018-02-05 15:16:13 +0800

    using adaptor functions

--- app/src/main/java/co/devpartners/devhub/ui/OpenSpaceActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/OpenSpaceActivity.kt
@@ -3,15 +3,25 @@ package co.devpartners.devhub.ui
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import co.devpartners.devhub.R
-import com.github.florent37.expansionpanel.viewgroup.ExpansionLayoutCollection
+import co.devpartners.devhub.expandable.adapter.ListAdapter
 import kotlinx.android.synthetic.main.activity_open_space.*
 
 
 class OpenSpaceActivity : AppCompatActivity() {
 
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_open_space)
 
+        val listHeader = listOf("Length Plan")
+        val numbersList = listOf("Hourly", "Daily", "Weekly")
+
+        val listChild = HashMap<String, List<String>>()
+        listChild.put(listHeader[0],numbersList)
+
+        val expandableListAdapter = ListAdapter(this, listHeader,listChild)
+
+        expandableView.setAdapter(expandableListAdapter)
     }
 }
