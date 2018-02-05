f9706960261df86909246630208fe198abf078f4
artjohn1993 <artjohn1993@gmail.com>
2018-02-05 10:35:45 +0800

    Add function for nextpage

--- app/src/main/java/co/devpartners/devhub/ui/SpaceTypeActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/SpaceTypeActivity.kt
@@ -4,6 +4,7 @@ import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import co.devpartners.devhub.R
 import kotlinx.android.synthetic.main.activity_space_type.*
+import org.jetbrains.anko.alert
 import org.jetbrains.anko.startActivity
 
 class SpaceTypeActivity : AppCompatActivity() {
@@ -28,11 +29,30 @@ class SpaceTypeActivity : AppCompatActivity() {
         }
 
         nextButton.setOnClickListener {
-            startActivity<OpenSpaceActivity>()
-            finish()
+            nextPage()
         }
     }
 
+    fun nextPage(){
+        when(selectedSpace){
+            ActivityType.OpenSpaceActivity -> {
+                startActivity<OpenSpaceActivity>()
+                finish()
+            }
+            ActivityType.ConferenceActivity ->{
+                startActivity<ConferenceActivity>()
+                finish()
+            }
+            ActivityType.PrivateActivity -> {
+
+            }
+            null -> {
+                alert("Please select space type"){
+                    title = "Information"
+                }.show()
+            }
+        }
+    }
 
     enum class ActivityType{
         OpenSpaceActivity,
