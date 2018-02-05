1229be411ab7f87b7d2a206313db1ca444071567
artjohn1993 <artjohn1993@gmail.com>
2018-02-05 14:41:55 +0800

    select Space Type

--- app/src/main/java/co/devpartners/devhub/ui/SpaceTypeActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/SpaceTypeActivity.kt
@@ -1,7 +1,11 @@
 package co.devpartners.devhub.ui
 
+import android.annotation.SuppressLint
+import android.graphics.Color
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import android.widget.RelativeLayout
 import co.devpartners.devhub.R
 import kotlinx.android.synthetic.main.activity_space_type.*
 import org.jetbrains.anko.alert
@@ -10,28 +14,49 @@ import org.jetbrains.anko.startActivity
 class SpaceTypeActivity : AppCompatActivity() {
     companion object {
         var selectedSpace : ActivityType? = null
+        var prevLayout : RelativeLayout? = null
+        var preImage : ImageView? = null
     }
 
+    @SuppressLint("ResourceAsColor")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_space_type)
 
         supportActionBar?.hide()
 
-        openSpace.setOnClickListener {
+        openSpaceLayout.setOnClickListener {
             selectedSpace = ActivityType.OpenSpaceActivity
+            selected(openSpaceLayout,openSpaceImage)
         }
-        privateSpace.setOnClickListener {
+        privateSpaceLayout.setOnClickListener {
             selectedSpace = ActivityType.PrivateActivity
+            selected(privateSpaceLayout,privateSpaceLogoImage)
         }
-        conferenceSpace.setOnClickListener {
+        conferenceSpaceLayout.setOnClickListener {
             selectedSpace = ActivityType.ConferenceActivity
+            selected(conferenceSpaceLayout,conferenceSpaceLogoImage)
         }
 
         nextButton.setOnClickListener {
             nextPage()
         }
     }
+
+    fun selected(layout : RelativeLayout , image : ImageView){
+
+        if(preImage != null && prevLayout != null){
+            preImage!!.setColorFilter(Color.argb(0, 255, 255, 255))
+            prevLayout!!.setBackgroundResource(R.drawable.button_border)
+        }
+
+        preImage = image
+        prevLayout = layout
+        layout.setBackgroundResource(R.color.colorPrimary)
+        image.setColorFilter(Color.argb(255, 255, 255, 255))
+    }
+
+
 
     fun nextPage(){
         when(selectedSpace){
@@ -53,7 +78,6 @@ class SpaceTypeActivity : AppCompatActivity() {
             }
         }
     }
-
     enum class ActivityType{
         OpenSpaceActivity,
         PrivateActivity,
