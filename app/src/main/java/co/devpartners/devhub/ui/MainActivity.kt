22e804ec74ed4d2ea4ab6c5958d13f1b455b43fb
artjohn1993 <artjohn1993@gmail.com>
2018-02-05 09:02:35 +0800

    Clean the code

--- app/src/main/java/co/devpartners/devhub/ui/MainActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/MainActivity.kt
@@ -13,7 +13,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         supportActionBar?.hide()
 
-        bookButton.setOnClickListener(){
+        bookButton.setOnClickListener{
             startActivity<RegisterActivity>()
         }
     }
