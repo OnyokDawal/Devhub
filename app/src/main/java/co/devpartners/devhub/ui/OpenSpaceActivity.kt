9ad3517a89d283f6fa6cd31ee3780f1817d17da8
OnyokDawal <junjun061096@gmail.com>
2018-02-07 12:37:15 +0800

    gikambyuan ang expandable og spinner ana lang. kani duha ka activity

--- app/src/main/java/co/devpartners/devhub/ui/OpenSpaceActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/OpenSpaceActivity.kt
@@ -1,26 +1,74 @@
 package co.devpartners.devhub.ui
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.view.MenuItem
+import android.view.View
+import android.widget.*
 import co.devpartners.devhub.R
-import co.devpartners.devhub.expandable.adapter.ListAdapter
 import kotlinx.android.synthetic.main.activity_open_space.*
+import java.util.*
+import java.util.Calendar.MONTH
+import javax.xml.datatype.DatatypeConstants.MONTHS
 
 
-class OpenSpaceActivity : AppCompatActivity() {
+class OpenSpaceActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
 
+
+    var timepick = arrayOf("Hourly", "Daily", "Weekly")
+
+    var spinner: Spinner? = null
+    var message: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_open_space)
 
-        val listHeader = listOf("Length Plan")
-        val numbersList = listOf("Hourly", "Daily", "Weekly")
-        val listChild = HashMap<String, List<String>>()
-        listChild.put(listHeader[0],numbersList)
 
-        val expandableListAdapter = ListAdapter(this, listHeader,listChild)
+        message = this.lengthMsg
+        spinner = this.spinnerView
+        spinnerView.onItemSelectedListener = this
 
-        expandableView.setAdapter(expandableListAdapter)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, timepick)
+        // Set layout to use when the list of choices appear
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Set Adapter to Spinner
+        spinnerView!!.adapter = adapter
+
+
+
+
+    }
+    override fun onNothingSelected(adapterView: AdapterView<*>?) {
+    }
+
+    override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, p3: Long) {
+        message!!.text = " Selected : "+timepick[position]
+    }
+    fun funDate(view : View){
+
+        val calendar = Calendar.getInstance()
+        calendar.add(Calendar.MONTH,+1)
+        val day = calendar.get(Calendar.DAY_OF_MONTH)
+        var month = calendar.get(MONTH)
+        val year = calendar.get(Calendar.YEAR)
+
+
+        val datepick = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view
+                                                                                   , year
+                                                                                   , monthOfYear
+                                                                                   , day ->
+
+
+            // Display Selected date in textbox
+
+            datepickPicker.text="$year/$monthOfYear/$day"
+        }, year, month, day)
+        datepick.show()
     }
 }
+
+
--- app/src/main/java/co/devpartners/devhub/ui/PrivateSpaceActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/PrivateSpaceActivity.kt
@@ -2,24 +2,43 @@ package co.devpartners.devhub.ui
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Spinner
+import android.widget.TextView
 import co.devpartners.devhub.R
-import co.devpartners.devhub.expandable.adapter.ListAdapter
 import kotlinx.android.synthetic.main.activity_private_space.*
 
-class PrivateSpaceActivity : AppCompatActivity() {
+class PrivateSpaceActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener{
 
+
+
+    var timepick = arrayOf("Hourly", "Daily", "Weekly")
+
+    var spinner: Spinner? = null
+    var message: TextView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_private_space)
 
-        val listHeader = listOf("Length Plan")
-        val numbersList = listOf("Hourly", "Daily", "Weekly")
-        val listChild = HashMap<String, List<String>>()
-        listChild.put(listHeader[0],numbersList)
+        message = this.lengthMsg
+        spinner = this.spinnerView
+        spinnerView.onItemSelectedListener = this
 
-        val expandableListAdapter = ListAdapter(this, listHeader, listChild)
-
-        expandableView.setAdapter(expandableListAdapter)
+        val adapter = ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, timepick)
+        // Set layout to use when the list of choices appear
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        // Set Adapter to Spinner
+        spinnerView!!.adapter = adapter
     }
+    override fun onNothingSelected(adapterView: AdapterView<*>?) {
+
+    }
+
+    override fun onItemSelected(adapterView: AdapterView<*>?, view: View?, position: Int, p3: Long) {
+        message!!.text = "Length Stay: "+timepick[position]
+    }
+
 }
