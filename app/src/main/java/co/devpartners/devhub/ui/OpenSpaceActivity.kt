d489136fc0d778d8d1c53dd2d93d1e54aaa872b5
OnyokDawal <junjun061096@gmail.com>
2018-02-07 14:14:14 +0800

    datepikor

--- app/src/main/java/co/devpartners/devhub/ui/OpenSpaceActivity.kt
+++ app/src/main/java/co/devpartners/devhub/ui/OpenSpaceActivity.kt
@@ -9,17 +9,18 @@ import android.view.MenuItem
 import android.view.View
 import android.widget.*
 import co.devpartners.devhub.R
+import co.devpartners.devhub.R.layout.datepicker
+import co.devpartners.devhub.api.events.CustomDatePickerDialog
+
 import kotlinx.android.synthetic.main.activity_open_space.*
-import java.util.*
-import java.util.Calendar.MONTH
-import javax.xml.datatype.DatatypeConstants.MONTHS
+
 
 
 class OpenSpaceActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener {
 
 
     var timepick = arrayOf("Hourly", "Daily", "Weekly")
-
+    val datepicker = CustomDatePickerDialog()
     var spinner: Spinner? = null
     var message: TextView? = null
 
@@ -27,6 +28,11 @@ class OpenSpaceActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_open_space)
 
+
+
+        datePickerButton.setOnClickListener{
+            datepicker.show(this)
+        }
 
         message = this.lengthMsg
         spinner = this.spinnerView
@@ -38,9 +44,6 @@ class OpenSpaceActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener
         // Set Adapter to Spinner
         spinnerView!!.adapter = adapter
 
-
-
-
     }
     override fun onNothingSelected(adapterView: AdapterView<*>?) {
     }
@@ -49,28 +52,7 @@ class OpenSpaceActivity : AppCompatActivity(),AdapterView.OnItemSelectedListener
         message!!.text = " Selected : "+timepick[position]
     }
 
-    //datepicker
-    fun funDate(view : View){
 
-        val calendar = Calendar.getInstance()
-        calendar.add(Calendar.MONTH,+1)
-        val day = calendar.get(Calendar.DAY_OF_MONTH)
-        var month = calendar.get(MONTH)
-        val year = calendar.get(Calendar.YEAR)
-
-
-        val datepick = DatePickerDialog(this, DatePickerDialog.OnDateSetListener { view
-                                                                                   , year
-                                                                                   , monthOfYear
-                                                                                   , day ->
-
-
-            // Display Selected date in textbox
-
-            datepickPicker.text="$year/$monthOfYear/$day"
-        }, year, month, day)
-        datepick.show()
-    }
 }
 
 
